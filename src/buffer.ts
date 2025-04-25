1dc97d4f05ee23f03aae1c873f41a2246dfa1428
cur10sDEV <specsx@protonmail.com>
2025-04-25 19:57:57 +0530

    update: tsconfig to use sharedArray with ES2020, and add some buffer examples

--- src/buffer.ts
+++ src/buffer.ts
@@ -1,5 +1,4 @@
-import { Buffer } from 'buffer';
-
+// import { Buffer } from "buffer";
 // const myBuffer = Buffer.alloc(3);
 
 // write as int
@@ -16,7 +15,36 @@ import { Buffer } from 'buffer';
 // const myBuffer = Buffer.from([0x48, 0x69, 0x21]);
 
 // or
-const myBuffer = Buffer.from('486921', 'hex');
+// const myBuffer = Buffer.from('486921', 'hex');
+// console.log(myBuffer);
+// console.log(myBuffer.toString('utf-8'));
 
-console.log(myBuffer);
-console.log(myBuffer.toString('utf-8'));
+// const buf = Buffer.from("This is a string");
+// const buf2 = new Uint8Array(16);
+
+// manipulation
+// buf[0] = 65;
+// buf2[0] = 255;
+
+// console.log(buf); // logs the hex representation
+// console.log(buf2); // logs the integer representation
+
+// logs the actual buffer - instance of ArrayBuffer
+// console.log(buf.buffer); // logs the full internal pool - 8KB
+// console.log(buf2.buffer);
+
+// get the actual data from the whole pool
+// console.log("start", buf.byteOffset);
+// console.log("end", buf.byteLength);
+
+// string representation
+// console.log(buf.toString("utf-8"));
+// console.log(buf2.toString());
+
+// Raw Buffer - really low level
+// you cant really do much with this
+// that is why you need a view to interact with it
+const rawBuf = new ArrayBuffer(20);
+const viewBuf = new Uint8Array(rawBuf);
+viewBuf[2] = 250;
+console.log(rawBuf); // data changed
